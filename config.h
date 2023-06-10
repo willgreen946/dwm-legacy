27883cd5848db96313b612c007a72b21fa96d6ea
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-10 09:24:41 +0100

    My fork of dwm

--- config.h
+++ config.h
@@ -78,6 +78,7 @@ static const Layout layouts[] = {
 #define SYSMON	 "htop"
 #define MYSITE   "torchweb.neocities.org"
 #define EMAIL_SITE "protonmail.com"
+#define BOOKMARKS  "/home/will/Documents/Bookmarks/bookmarks.html"
 #define BRIGHTNESS_PATH "/sys/class/backlight/nv_backlight/brightness"
 /* Note certain key chains are in place to allow for alot more keybinds
 alt + f will allow for dwm function for example quitting dwm or killing a program
@@ -85,70 +86,71 @@ alt + w will allow to open certain pages within a browser */
 
 static const Key keys[] = {
 	/* modifier                     chain key key        function        argument */
-	{ MODKEY,             		-1,       XK_space,  spawn,          {.v = (const char*[]) {TERMINAL, 	       			NULL}}},
-	{ MODKEY|ShiftMask,		-1,	  XK_space,  spawn,	     {.v = (const char*[]) {"tabbed", "-c", "-r", "2", TERMINAL, "-w", """",NULL}}},
-	{ MODKEY,			-1, 	  XK_f,      spawn,	     {.v = (const char*[]) {TERMINAL, FM,      			NULL}}},
-	{ MODKEY,			XK_p,	  XK_m,	     spawn,	     {.v = (const char*[]) {TERMINAL, SYSMON,			NULL}}},
-	{ MODKEY,			XK_w,	  XK_b,	     spawn,	     {.v = (const char*[]) {BROWSER,  SEARCH,  	        	NULL}}},
-	{ MODKEY,			XK_w,     XK_y,	     spawn,	     {.v = (const char*[]) {BROWSER, "yewtu.be", 		NULL}}},
-	{ MODKEY,			XK_w,	  XK_e,	     spawn,	     {.v = (const char*[]) {BROWSER, EMAIL_SITE,		NULL}}},
-	{ MODKEY,			XK_w,	  XK_f,	     spawn,	     {.v = (const char*[]) {BROWSER, "forums.freebsd.org",	NULL}}},
-	{ MODKEY,			XK_w,	  XK_l,	     spawn,	     {.v = (const char*[]) {BROWSER, "lemmy.ml",		NULL}}},
-	{ MODKEY,			XK_w,	  XK_c,	     spawn,	     {.v = (const char*[]) {BROWSER, "cboard.cprogramming.com", NULL}}},
-	{ MODKEY,			XK_w,	  XK_o,	     spawn,	     {.v = (const char*[]) {BROWSER, "man.openbsd.org",		NULL}}},
-	{ MODKEY,			XK_w,	  XK_g,	     spawn,	     {.v = (const char*[]) {BROWSER, "github.com/africavoid", 	NULL}}},
-	{ MODKEY,			XK_w,	  XK_t,	     spawn,	     {.v = (const char*[]) {BROWSER, "forums.gentoo.org",	NULL}}},
-	{ MODKEY,			XK_w,	  XK_r,	     spawn,	     {.v = (const char*[]) {BROWSER, "old.reddit.com",		NULL}}},
-	{ MODKEY,			XK_w,	  XK_a,	     spawn,	     {.v = (const char*[]) {BROWSER, "wiki.archlinux.org",	NULL}}},
-	{ MODKEY,			XK_w,	  XK_s,	     spawn,	     {.v = (const char*[]) {BROWSER, "suckless.org",		NULL}}},
-	{ MODKEY,			XK_w,	  XK_w,	     spawn,	     {.v = (const char*[]) {BROWSER, "wiby.me",			NULL}}},
-	{ MODKEY,			XK_w,	  XK_m,	     spawn,	     {.v = (const char*[]) {BROWSER, MYSITE,			NULL}}},
-	{ MODKEY,			XK_p,	  XK_p,	     spawn,	     {.v = (const char*[]) {"dmenu_run",			NULL}}},
+	{ MODKEY,           	-1,       XK_space,  spawn,	{.v = (const char*[]) {TERMINAL, 	       			 NULL}}},
+	{ MODKEY|ShiftMask,	-1,	  XK_space,  spawn,	{.v = (const char*[]) {"tabbed", "-c", "-r", "2", TERMINAL, "-w", """",NULL}}},
+	{ MODKEY,		-1, 	  XK_f,      spawn,	{.v = (const char*[]) {TERMINAL, FM,     			 NULL}}},
+	{ MODKEY,		XK_p,	  XK_m,	     spawn,	{.v = (const char*[]) {TERMINAL, SYSMON, 			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_space,  spawn,	{.v = (const char*[]) {BROWSER,  SEARCH, 			 NULL}}},
+	{ MODKEY,		XK_w,     XK_y,	     spawn,	{.v = (const char*[]) {BROWSER, "yewtu.be", 			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_e,	     spawn,	{.v = (const char*[]) {BROWSER, EMAIL_SITE, 			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_f,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.freebsd.org", 		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_l,	     spawn,	{.v = (const char*[]) {BROWSER, "lemmy.ml", 		   	 NULL}}},
+	{ MODKEY,		XK_w,	  XK_c,	     spawn,	{.v = (const char*[]) {BROWSER, "cboard.cprogramming.com", 	 NULL}}},
+	{ MODKEY,		XK_w,	  XK_o,	     spawn,	{.v = (const char*[]) {BROWSER, "man.openbsd.org", 		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_g,	     spawn,	{.v = (const char*[]) {BROWSER, "github.com/africavoid", 	 NULL}}},
+	{ MODKEY,		XK_w,	  XK_t,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.gentoo.org", 		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_r,	     spawn,	{.v = (const char*[]) {BROWSER, "old.reddit.com", 		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_a,	     spawn,	{.v = (const char*[]) {BROWSER, "wiki.archlinux.org", 		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_s,	     spawn,	{.v = (const char*[]) {BROWSER, "suckless.org",			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_w,	     spawn,	{.v = (const char*[]) {BROWSER, "wiby.me", 			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_m,	     spawn,	{.v = (const char*[]) {BROWSER, MYSITE,				 NULL}}},
+	{ MODKEY,		XK_w,	  XK_b,	     spawn,	{.v = (const char*[]) {BROWSER, BOOKMARKS,			 NULL}}},
+	{ MODKEY,		XK_p,	  XK_p,	     spawn,	{.v = (const char*[]) {"dmenu_run",				 NULL}}},
 	#ifdef __linux 
-	{ MODKEY,                       XK_a,     XK_k,      spawn,          {.v = (const char*[]) {"amixer",   "set", "Master", "10%+",    NULL}}},
-        { MODKEY,                       XK_a,     XK_j,      spawn,          {.v = (const char*[]) {"amixer",   "set", "Master", "10%-",    NULL}}},
-        { MODKEY,                       XK_a,     XK_m,      spawn,          {.v = (const char*[]) {"amixer",   "set", "Master", "mute",    NULL}}},
-        { MODKEY,                       XK_a,     XK_u,      spawn,          {.v = (const char*[]) {"amixer",   "set", "Master", "unmute",  NULL}}},
-        { MODKEY,                       XK_a,     XK_a,      spawn,          {.v = (const char*[]) {TERMINAL,"alsamixer",		    NULL}}},
-	{ MODKEY,			XK_m,	  XK_k,	     spawn,	     {.v = (const char*[]) {"sh","echo", "100", ">", BRIGHTNESS_PATH, NULL}}},
-	{ MODKEY,			XK_m,	  XK_j,	     spawn,	     {.v = (const char*[]) {"sh","echo", "30", ">", BRIGHTNESS_PATH, NULL}}}, 
+	{ MODKEY,               XK_a,     XK_k,      spawn,     {.v = (const char*[]) {"amixer",   "set", "Master", "10%+",      NULL}}},
+        { MODKEY,               XK_a,     XK_j,      spawn,     {.v = (const char*[]) {"amixer",   "set", "Master", "10%-",      NULL}}},
+        { MODKEY,               XK_a,     XK_m,      spawn,     {.v = (const char*[]) {"amixer",   "set", "Master", "mute",      NULL}}},
+        { MODKEY,               XK_a,     XK_u,      spawn,     {.v = (const char*[]) {"amixer",   "set", "Master", "unmute",    NULL}}},
+        { MODKEY,               XK_a,     XK_a,      spawn,     {.v = (const char*[]) {TERMINAL,"alsamixer",		       	 NULL}}},
+	{ MODKEY,		XK_m,	  XK_k,	     spawn,	{.v = (const char*[]) {"sh","echo", "100", ">", BRIGHTNESS_PATH, NULL}}},
+	{ MODKEY,		XK_m,	  XK_j,	     spawn,	{.v = (const char*[]) {"sh","echo", "30", ">", BRIGHTNESS_PATH,  NULL}}}, 
 	#endif 
 	#ifdef __OpenBSD__ 
-	{ MODKEY,			XK_a,	  XK_k,	     spawn,	     {.v = (const char*[]) {"sndioctl", "output.level=+0.1",	NULL}}},
-	{ MODKEY,			XK_a,	  XK_j,	     spawn,	     {.v = (const char*[]) {"sndioctl", "output.level=-0.1",	NULL}}},
-	{ MODKEY,			XK_a,	  XK_m,	     spawn,	     {.v = (const char*[]) {"sndioctl", "output.mute=1",	NULL}}},
-	{ MODKEY,			XK_a,	  XK_u,	     spawn,	     {.v = (const char*[]) {"sndioctl", "output.mute=0",	NULL}}},
+	{ MODKEY,		XK_a,	  XK_k,	     spawn,	{.v = (const char*[]) {"sndioctl", "output.level=+0.1",	NULL}}},
+	{ MODKEY,		XK_a,	  XK_j,	     spawn,	{.v = (const char*[]) {"sndioctl", "output.level=-0.1",	NULL}}},
+	{ MODKEY,		XK_a,	  XK_m,	     spawn,	{.v = (const char*[]) {"sndioctl", "output.mute=1",	NULL}}},
+	{ MODKEY,		XK_a,	  XK_u,	     spawn,	{.v = (const char*[]) {"sndioctl", "output.mute=0",	NULL}}},
 	#endif
-	{ MODKEY,			XK_s,     XK_b,	     togglebar,	     {0}},
-	{ MODKEY,			XK_s,	  XK_s,	     schemeCycle,    {0}},
-	{ MODKEY,                       -1,	  XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       -1,	  XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       -1,       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       -1,       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             -1,       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,             -1,	  XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,             -1,	  XK_k,      setcfact,       {.f = +0.25} },
-	{ MODKEY|ShiftMask,             -1,	  XK_j,      setcfact,       {.f = -0.25} },
-	{ MODKEY,             		XK_s,     XK_r,      setcfact,       {.f =  0.00} },
-	{ MODKEY,                       -1,       XK_Return, zoom,           {0} },
-	{ MODKEY,                       -1,       XK_Tab,    view,           {0} },
-	{ MODKEY,             		XK_s,     XK_k,      killclient,     {0} },
-	{ MODKEY,             		XK_s,     XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,             		XK_s,	  XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,			XK_r,	  XK_h,      resizemouse,    {0} },
-	{ MODKEY,			XK_r,	  XK_l,	     resizemouse,    {0} },
-	{ MODKEY,			XK_r,	  XK_j,	     movemouse,	     {0} },
-	{ MODKEY,			XK_r,	  XK_k,	     movemouse,	     {0} },
-	{ MODKEY,			XK_r,	  XK_r,	     togglefloating, {0} },
-	TAGKEYS(                        -1, 	  XK_1,                    0)
-	TAGKEYS(                        -1,	  XK_2,                    1)
-	TAGKEYS(                        -1,       XK_3,                    2)
-	TAGKEYS(			-1,       XK_4,			   3)
-	TAGKEYS(			-1,       XK_5,			   4)
-	TAGKEYS(			-1,	  XK_6,			   5)
-	TAGKEYS(			-1,	  XK_7,			   6)
-	TAGKEYS(			-1,	  XK_8,			   7)
-	{ MODKEY,                       XK_s,     XK_q,      quit,           {0} },
+	{ MODKEY,		XK_s,     XK_b,	     togglebar,	     {0}},
+	{ MODKEY,		XK_s,	  XK_s,	     schemeCycle,    {0}},
+	{ MODKEY,               -1,	  XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,               -1,	  XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,               -1,       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,               -1,       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,     -1,       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,     -1,	  XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,     -1,	  XK_k,      setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,     -1,	  XK_j,      setcfact,       {.f = -0.25} },
+	{ MODKEY,             	XK_s,     XK_r,      setcfact,       {.f =  0.00} },
+	{ MODKEY,               -1,       XK_Return, zoom,           {0} },
+	{ MODKEY,               -1,       XK_Tab,    view,           {0} },
+	{ MODKEY,             	XK_s,     XK_k,      killclient,     {0} },
+	{ MODKEY,             	XK_s,     XK_t,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,             	XK_s,	  XK_m,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,		XK_r,	  XK_h,      resizemouse,    {0} },
+	{ MODKEY,		XK_r,	  XK_l,	     resizemouse,    {0} },
+	{ MODKEY,		XK_r,	  XK_j,	     movemouse,	     {0} },
+	{ MODKEY,		XK_r,	  XK_k,	     movemouse,	     {0} },
+	{ MODKEY,		XK_r,	  XK_r,	     togglefloating, {0} },
+	TAGKEYS(                -1, 	  XK_1,                    0)
+	TAGKEYS(                -1,	  XK_2,                    1)
+	TAGKEYS(                -1,       XK_3,                    2)
+	TAGKEYS(		-1,       XK_4,			   3)
+	TAGKEYS(		-1,       XK_5,			   4)
+	TAGKEYS(		-1,	  XK_6,			   5)
+	TAGKEYS(		-1,	  XK_7,			   6)
+	TAGKEYS(		-1,	  XK_8,			   7)
+	{ MODKEY,               XK_s,     XK_q,      quit,           {0} },
 		
 };
 
