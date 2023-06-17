cae9454d4edd3ae4bdec0e95bcb5ba546b50c518
Africa Void <72352225+africavoid@users.noreply.github.com>
2023-06-17 22:19:06 +0100

    My fork of dwm

--- config.def.h
+++ config.def.h
@@ -9,31 +9,37 @@ static const int user_bh	    = 16;
 static const char *fonts[]          = { "terminus:size=8" };
 static const char dmenufont[]       = "terminus:size=8";
 static const char black[]      	    = "#000000";
-static const char purple[]          = "#54487a"; // Purple 
-static const char white[]           = "#ffffff"; // White
-static const char yellow[]          = "#f2ca30"; // Yellow
-static const char orange[]          = "#f26711"; // Orange
+static const char red[]		    = "#eb0028";
+static const char purple[]          = "#54487a"; 
+static const char white[]           = "#ffffff";
+static const char yellow[]          = "#f2ca30";
+static const char orange[]          = "#f26711";
 static const char matrix_green[]    = "#008f11";
 static const char matrix_dark[]	    = "#003b00";
 
 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
+	// Orange Theme
+       	{ white, black, black },  
+       	{ black, orange, orange},
+	
+	// Matrix Theme
 	{ matrix_green, black, matrix_dark },
 	{ black, matrix_green, matrix_green },
+
 	// Yellow Theme
         { white, black, purple }, 
         { black, yellow,yellow  },
 	
-	// Orange Theme
-       	{ white, black, black },     /* SchemeNorm light */
-       	{ black, orange, orange},      /* SchemeSel light */
+	// Red Theme
+       	{ white, black, black },  
+       	{ black, red, red},
 };
 
 /* tagging */
 static const char *tags[] = { "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]"};
 
-#define BROWSER "qutebrowser"
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -68,7 +74,7 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
+#define BROWSER "qutebrowser"
 #define SEARCH   "searx.be"
 #define SEARCH_ALT "searx.org"
 #define TERMINAL "st"
@@ -79,6 +85,8 @@ static const Layout layouts[] = {
 #define EMAIL_SITE "protonmail.com"
 #define BOOKMARKS  "/home/will/Documents/Bookmarks/bookmarks.html"
 #define BRIGHTNESS_PATH "/sys/class/backlight/nv_backlight/brightness"
+#define WIFISCRIPT "/home/will/.scripts"
+#define NTWRK "wlan0"
 /* Note certain key chains are in place to allow for alot more keybinds
 alt + f will allow for dwm function for example quitting dwm or killing a program
 alt + w will allow to open certain pages within a browser */
@@ -93,21 +101,28 @@ static const Key keys[] = {
 	{ MODKEY,		XK_p,	  XK_p,	     spawn,	{.v = (const char*[]) {"dmenu_run",				 NULL}}},
 	/* scripts */
 	#ifdef __linux
-	{ MODKEY,		XK_s,	  XK_n,	     spawn,	{.v = (const char*[]) {TERMINAL, "sh", "/home/will/.scripts/wifi.sh", "-o", "linux", "-i", "wlan0", NULL}}},
-	{ MODKEY,		XK_s,	  XK_b,	     spawn,	{.v = (const char*[]) {TERMINAL, "doas", "nvim", "/sys/class/backlight/nv_backlight/brightness",    NULL}}},
+	{ MODKEY,		XK_s,	  XK_n,	     spawn,	{.v = (const char*[]) {TERMINAL, "sh", WIFISCRIPT, "-o", "linux", "-i", NTWK , NULL}}},
 	#endif
 	#ifdef __OpenBSD__
 	{ MODKEY,		XK_s,	  XK_n,	     spawn,	{.v = (const char*[]) {TERMINAL, "doas", "sh", "home/will/.scripts/obsd.sh", NULL}}},
 	#endif
-	{ MODKEY,		XK_s,	  XK_w,	     spawn,	{.v = (const char*[]) {TERMINAL, "curl", "wttr.in", "&&", "read i", NULL}}},
+	{ MODKEY,		XK_s,	  XK_w,	     spawn,	{.v = (const char*[]) {TERMINAL, "bash", "curl", "wttr.in", "&&", "read i", NULL}}},
 	/* web  XK_w is normal websites XK_o is other websites since theres a lot of websites I want to have binds for*/
 	{ MODKEY,		XK_w,	  XK_1,      spawn,	{.v = (const char*[]) {BROWSER,  SEARCH, 			 NULL}}},
 	{ MODKEY,		XK_w,     XK_2,	     spawn,	{.v = (const char*[]) {BROWSER, "yewtu.be", 			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_3,	     spawn,	{.v = (const char*[]) {BROWSER,	"wiki.installgentoo.com",	 NULL}}},
+	{ MODKEY,		XK_w,	  XK_4,	     spawn,	{.v = (const char*[]) {BROWSER,	"cboard.cprogramming.com",	 NULL}}},
+	{ MODKEY,		XK_w,	  XK_5,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.gentoo.org",		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_6,	     spawn,	{.v = (const char*[]) {BROWSER, "bbs.archlinux.org",		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_7,	     spawn,	{.v = (const char*[]) {BROWSER,	"openbsd.org",			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_8,	     spawn,	{.v = (const char*[]) {BROWSER,	"gnu.org",			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_9,	     spawn,	{.v = (const char*[]) {BROWSER, "ebay.co.uk",			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_0,	     spawn,	{.v = (const char*[]) {BROWSER, "bbc.com/iplayer",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_e,	     spawn,	{.v = (const char*[]) {BROWSER, EMAIL_SITE, 			 NULL}}},
 	{ MODKEY,		XK_w,	  XK_f,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.freebsd.org", 		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_l,	     spawn,	{.v = (const char*[]) {BROWSER, "lemmy.ml", 		   	 NULL}}},
 	{ MODKEY,	 	XK_w,	  XK_c,	     spawn,	{.v = (const char*[]) {BROWSER, "cprogramming.com",		 NULL}}},
-	{ MODKEY,		XK_w,	  XK_r,  spawn,	{.v = (const char*[]) {BROWSER, "cppreference.com",		 NULL}}},
+	{ MODKEY,		XK_w,	  XK_r,      spawn,	{.v = (const char*[]) {BROWSER, "cppreference.com",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_minus,  spawn,	{.v = (const char*[]) {BROWSER, "legacy.cplusplus.com/",	 NULL}}},
 	{ MODKEY,		XK_w,	  XK_o,	     spawn,	{.v = (const char*[]) {BROWSER, "man.openbsd.org", 		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_g,	     spawn,	{.v = (const char*[]) {BROWSER, "github.com/africavoid", 	 NULL}}},
@@ -117,17 +132,12 @@ static const Key keys[] = {
 	{ MODKEY,		XK_w,	  XK_w,	     spawn,	{.v = (const char*[]) {BROWSER, "wiby.me", 			 NULL}}},
 	{ MODKEY,		XK_w,	  XK_m,	     spawn,	{.v = (const char*[]) {BROWSER, MYSITE,				 NULL}}},
 	{ MODKEY,		XK_w,	  XK_b,	     spawn,	{.v = (const char*[]) {BROWSER, BOOKMARKS,			 NULL}}},
-	{ MODKEY,		XK_w,	  XK_0,	     spawn,	{.v = (const char*[]) {BROWSER, "bbc.com/iplayer",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_space,  spawn,	{.v = (const char*[]) {BROWSER, SEARCH_ALT,			 NULL}}},
-	{ MODKEY,		XK_w,	  XK_3,	     spawn,	{.v = (const char*[]) {BROWSER, "vid.puffyan.us",		 NULL}}},
-	{ MODKEY,		XK_w,	  XK_y,	     spawn,	{.v = (const char*[]) {BROWSER,	"yewtu.be",			 NULL}}},
+	{ MODKEY,		XK_w,	  XK_y,	     spawn,	{.v = (const char*[]) {BROWSER, "vid.puffyan.us",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_q,	     spawn,	{.v = (const char*[]) {BROWSER,	"linuxquestions.org",		 NULL}}},
-	{ MODKEY,		XK_w,	  XK_4,	     spawn,	{.v = (const char*[]) {BROWSER,	"cboard.cprogramming.com",	 NULL}}},
 	{ MODKEY,		XK_w,	  XK_equal,  spawn,	{.v = (const char*[]) {BROWSER, "legacy.cplusplus.com/forum",	 NULL}}},
-	{ MODKEY,		XK_w,	  XK_5,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.gentoo.org",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_d,	     spawn,	{.v = (const char*[]) {BROWSER, "daemonforums.org",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_n,	     spawn,	{.v = (const char*[]) {BROWSER,	"https://www.linuxquestions.org/questions/%2Absd-17/", NULL}}},
-	{ MODKEY,		XK_w,	  XK_6,	     spawn,	{.v = (const char*[]) {BROWSER, "forums.archlinux.org",		 NULL}}},
 	{ MODKEY,		XK_w,	  XK_k,	     spawn,	{.v = (const char*[]) {BROWSER, "wiki.linuxquestions.org/wiki/Main_Page", NULL}}},
 	#ifdef __linux 
 	/* Linux specific commands */
@@ -168,8 +178,8 @@ static const Key keys[] = {
 	/* resize */
 	{ MODKEY,		XK_r,	  XK_h,      resizemouse,    {0}},
 	{ MODKEY,		XK_r,	  XK_l,	     resizemouse,    {0}},
-	{ MODKEY,		XK_r,	  XK_j,	     movemouse,	     {0}},
-	{ MODKEY,		XK_r,	  XK_k,	     movemouse,	     {0}},
+	{ MODKEY,		XK_r,	  XK_j,	     movemouse,	     {.i = -1}},
+	{ MODKEY,		XK_r,	  XK_k,	     movemouse,	     {.i = +1}},
 	{ MODKEY,		XK_r,	  XK_r,	     togglefloating, {0}},
 	/* move between tags */
 	TAGKEYS(                -1, 	  XK_1,                    0)
